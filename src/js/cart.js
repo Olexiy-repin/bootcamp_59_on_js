288207084f616da55ef120bb526ba6d900b66566
Olexiy-repin <repinlyoha@gmail.com>
2023-09-06 23:03:11 +0300

    preparation for classwork

---
+++ src/js/cart.js
@@ -0,0 +1,7 @@
+/* <li class="products-item">
+  <img src="" alt="" class="products-img" />
+  <div class="products-content">
+    <h3 class="products-title"></h3>
+    <p class="products-description"></p>
+  </div>
+</li>; */
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+/* 
+<li class="products-item">
+      <img src="" alt="" class="products-img" />
+      <div class="products-content">
+        <h3 class="products-title"></h3>
+        <p class="products-description"></p>
+        <p class="products-rating"></p>
+        <span class="products-price"></span>
+        <button class="products-add-to-cart-btn" type="button"></button>
+      </div>
+    </li> 
+*/
