7d84eb568a7bc7349c86bf4d619aa3f92c90389f
Olexiy-repin <repinlyoha@gmail.com>
2023-09-07 11:42:43 +0300

    classwork

--- src/js/micro-macro-tasks.js
+++ src/js/micro-macro-tasks.js
@@ -1,5 +1,25 @@
 // http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ2ZpcnN0Jyk7CgpzZXRUaW1lb3V0KGZ1bmN0aW9uKCkgewogICAgY29uc29sZS5sb2coJ2lubmVyIHNldFRpbWVvdXQnKTsKfSwgMTAwMCk7Cgpjb25zb2xlLmxvZygnc2Vjb25kJyk7!!!
 
+// console.log('Start!');
+
+// setTimeout(() => {
+//   console.log('Hello from setTimeout!');
+// }, 0);
+
+// const promise = new Promise((resolve, reject) => {
+//   console.log('Hello from class Promise!');
+
+//   setTimeout(() => {
+//     resolve('Done!');
+//   }, 2000);
+// });
+
+// promise.then(result => {
+//   console.log(result);
+// });
+
+// console.log('End!');
+
 //? TASK 01
 // console.log(1);
 
@@ -16,4 +36,36 @@
 // console.log(7);
 
 //? TASK 02
+
+//? Лічильник через рекурсію
 // const counterOutputEl = document.querySelector('.js-counter');
+// let i = 0;
+
+// const count = () => {
+//   i += 100;
+
+//   counterOutputEl.innerHTML = i;
+
+//   if (i !== 10000) {
+//     setTimeout(count, 10);
+//   }
+// };
+
+// count();
+
+//? Лічильник через setInterval
+// const counterOutputEl = document.querySelector('.js-counter');
+// let intervalId = null;
+// let i = 0;
+
+// const count = () => {
+//   i += 10;
+
+//   counterOutputEl.innerHTML = i;
+
+//   if (i === 1000) {
+//     clearInterval(intervalId);
+//   }
+// };
+
+// intervalId = setInterval(count, 100);
